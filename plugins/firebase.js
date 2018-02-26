4a0b4b157e3fc5e624fc1f9200fe208f78bd1f49
brandon-schabel <brandonschabel1995@gmail.com>
2018-02-26 05:28:23 -0800

    update firebase config

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,15 +1,8 @@
 import * as firebase from 'firebase'
-import store from '~/store'
+import config from './config'
 
 if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: 'AIzaSyC61qoH8POsOd51vjB5LyFL6tiefOnRcGY',
-    authDomain: 'react-firebase-starter-c28c7.firebaseapp.com',
-    databaseURL: 'https://react-firebase-starter-c28c7.firebaseio.com',
-    projectId: 'react-firebase-starter-c28c7',
-    storageBucket: 'react-firebase-starter-c28c7.appspot.com',
-    messagingSenderId: '271307299986'
-  })
+  firebase.initializeApp(config)
 }
 
 export default (context) => {
